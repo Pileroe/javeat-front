2ec3021cf23dc18c0c599b336889a6d3bc424cf5
Alessandro <irelip.a@gmail.com>
2024-03-01 15:35:40 +0100

    1

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,13 +17,13 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <nav className="navbar navbar-expand-lg justify-content-between" style={{ backgroundColor: "#000000", height: "150px" }}> 
             <div className="container-fluid m-0 px-5">
-                <Link to="/" className="navbar-brand">
-                    <img src="https://cdn.discordapp.com/attachments/1211972312690069504/1212065840694952017/9171171b-36ac-4796-8f2e-e1f97bf276e1.png?ex=65f07b47&is=65de0647&hm=44281128b2b1c67c51a380dddb53966a6e22180dcaf9790cc05f8432937ca93e&" alt="Logo" height="60" />
+            <Link  to="/" className="navbar-brand ">
+                    <img src="https://cdn.discordapp.com/attachments/1211972312690069504/1213091514042028073/final.png?ex=65f43683&is=65e1c183&hm=9ab0c4f65d0c2ae426bab753481a4e6ee5927d8bb367ca42bb23f0d00a8ea2b9&"alt="Logo" height="180" />
                 </Link>
-                <div className="navbar-nav ms-auto">
-                    <Link className="nav-link ms-2" to="/allrestaurants">All Restaurants</Link>
+                <div className="navbar-nav ">
+                <Link className="nav-link ms-2 text-white fst-italic fw-semibold fs-5" to="/allrestaurants">All Restaurants</Link>
                     {!isUserNotEmpty ? (
                         <Link className="nav-link text-white fst-italic fw-semibold fs-5" to="user/login">Login</Link>
                     ) : (
