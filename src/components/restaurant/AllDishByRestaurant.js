d98c3cb5174fcbe93f4b72f2b753894c0966f291
ciotoliveronica <veronicaciotolivc@gmail.com>
2024-03-05 16:33:27 +0100

    version3

--- src/components/restaurant/AllDishByRestaurant.js
+++ src/components/restaurant/AllDishByRestaurant.js
@@ -1,8 +1,14 @@
+import { useAtom } from "jotai";
+import { currentUser } from "../../App";
+
 const AllDishByRestaurant =() =>
 {
+    const [user] = useAtom(currentUser);
     return(
         <>
         
         </>
     );
 }
+
+export default AllDishByRestaurant;
--- src/components/restaurant/RestaurantByOwner.js
+++ src/components/restaurant/RestaurantByOwner.js
@@ -23,7 +23,36 @@ const RestaurantByOwner = () =>{
 
       return(
         <>
-            
+            <div className='mx-4'>
+                <div className="card h-100 border border-dark rounded">
+                    {restaurant && (
+                        <Link to={`/restaurants/${restaurant.id}`}>
+                            <img
+                                src={restaurant.imgUrl}
+                                alt="Restaurant"
+                                className="card-img-top"
+                                style={{
+                                    height: "20vh",
+                                    width: "100%",
+                                    objectFit: "cover", 
+                                    objectPosition: "center" 
+                                }}
+                            />
+                        </Link>
+                    )}
+                    <div className="card-body">
+                        {restaurant && (
+                            <>
+                                <h5 className="card-title">{restaurant.name}</h5>
+                                <p className="card-text"><strong>Open:</strong> {restaurant.is_open ? 'Yes' : 'No'}</p>
+                                <p className="card-text"><strong>Food Types:</strong> {restaurant.foodTypes.join(', ')}</p>
+                                <p className="card-text"><strong>Distance:</strong> {Math.round(distance)} Km</p>
+                                <button type="button" class="btn btn-light">Detail Restaurant</button>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
