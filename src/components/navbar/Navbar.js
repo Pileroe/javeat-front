b06096f268ce49c47345fbd734298972695c5f0e
ciotoliveronica <veronicaciotolivc@gmail.com>
2024-03-06 12:05:20 +0100

    veronicaMerc

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg justify-content-between" style={{ backgroundColor: "#000000", height: "100px" }}>
-            <div className="container-fluid m-0 px-5">
+            <div className="container-fluid m-5 px-5">
                 <Link to="/" className="navbar-brand ">
                     <img src="/finale_maybe.png" alt="Logo" height="200" />
                 </Link>
--- src/components/restaurant/MyDishes.js
+++ src/components/restaurant/MyDishes.js
@@ -3,31 +3,45 @@ import axios from 'axios';
 
 const MyDishes = (props) => {
     const [dishes, setDishes] = useState([]);
-    
+
     useEffect(
-        ()=>{
+        () => {
             axios.get(`/dishes/${props.restaurant.id}`)
-            .then((response)=>{
-                setDishes(response.data);
-            }).catch((error)=>{
+                .then((response) => {
+                    setDishes(response.data);
+                }).catch((error) => {
 
-                console.error('Error fetching dishes:', error);
+                    console.error('Error fetching dishes:', error);
 
-            });
+                });
         },
         [props.flicker]
     );
+
+    // src="/static/burger.jpg"
     return (
-        <div>
-            {dishes.map((dish) => (
-                <div key={dish.id} className="dish-card">
-                    <h4>{dish.name}</h4>
-                    <p>{dish.description}</p>
-                    <p>{dish.category}</p>
-                    <p>Price: ${dish.price}</p>
-                </div>
-            ))}
+        <div className="container-fluid">
+            <div className="row">
+                {dishes.map((dish) => (
+                    <div key={dish.id} className="col-md-6 mb-4">
+                        <div className="card flex-column d-flex">
+                            <img src="/static/burger.jpg" className="card-img-top w-100" style={{ objectFit: 'cover', height: '200px' }} alt="..." />
+                            <div className="card-body d-flex flex-column">
+                                <h5 className="card-title text-truncate custom-font">{dish.name}</h5>
+                                <p className="card-text custom-font">{dish.description}</p>
+                                <p className="card-text custom-font"><small className="text-muted">{dish.category}</small></p>
+                                <p className="card-text custom-font">Price: ${dish.price}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
+
+
+
+
+
     );
 };
 
